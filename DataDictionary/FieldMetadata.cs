1d479075feded4443432858b74a79a29358e28ec
Lou Rainaldi <lourainaldi@microsoft.com>
2025-06-04 10:06:09 -0400

    Add project files.

---
+++ DataDictionary/FieldMetadata.cs
@@ -0,0 +1,9 @@
+public class FieldMetadata
+{
+    public string EntityName { get; set; }
+    public string SchemaName { get; set; }
+    public string DisplayName { get; set; }
+    public string Type { get; set; }
+    public string RequiredLevel { get; set; }
+    public string Description { get; set; }
+}
