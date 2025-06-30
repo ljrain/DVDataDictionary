168e6bb26bd6c4730e2a669f6f6a7d8f052e21b4
Lou Rainaldi <lourainaldi@microsoft.com>
2025-06-30 10:25:00 -0400

    commented out unused properties

--- DataDictionaryProcessor/Models/DataDictionaryAttribute.cs
+++ DataDictionaryProcessor/Models/DataDictionaryAttribute.cs
@@ -10,23 +10,23 @@ namespace DataDictionary.Models
     public class DataDictionaryAttribute
     {
         public string FullName { get; set; }
-        public Guid AttributeId { get; set; }
+        //public Guid AttributeId { get; set; }
         public Guid AttributeOf { get;set; }
         public string AttributeName { get; set; }
-        public Guid AttributeTypeId { get; set; }
-        public string AttributeTypeName { get; set; }
-        public string Description { get; set; }
-        public bool IsPrimaryKey { get; set; }
-        public bool IsRequired { get; set; }
-        public bool IsValidForCreate { get; set; }
-        public bool IsValidForUpdate { get; set; }
-        public int MaxLength { get; set; }
-        public int MinValue { get; set; }
-        public int MaxValue { get; set; }
-        public string OptionSetName { get; set; }
+        //public Guid AttributeTypeId { get; set; }
+        //public string AttributeTypeName { get; set; }
+        //public string Description { get; set; }
+        //public bool IsPrimaryKey { get; set; }
+        //public bool IsRequired { get; set; }
+        //public bool IsValidForCreate { get; set; }
+        //public bool IsValidForUpdate { get; set; }
+        //public int MaxLength { get; set; }
+        //public int MinValue { get; set; }
+        //public int MaxValue { get; set; }
+        //public string OptionSetName { get; set; }
         public string LogicalName { get; set; }
-        public string PhysicalName { get; set; }
-        public string ComponentStateName { get; set; }
+        //public string PhysicalName { get; set; }
+        //public string ComponentStateName { get; set; }
 
         public DataDictionaryAttributeMetadata Metadata { get; set; } = new DataDictionaryAttributeMetadata();
 
